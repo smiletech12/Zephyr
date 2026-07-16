95beec37443b00584a117eba54ba7735f0647070
MR SMILE <162684003+smiletech12@users.noreply.github.com>
2026-07-17 00:55:19 +0300

    Create config.js for bot settings

---
+++ config.js
@@ -0,0 +1,11 @@
+export default {
+  developer: "DEV SMILE",
+  botName: "ZEPHYR",
+  botVersion: "1.0.0",
+  prefix: ".",
+  timezone: "Asia/Kolkata",
+  autoRead: true,
+  autoStatus: true,
+  autoTyping: true,
+  modePublic: true,
+};
